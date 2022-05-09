6c470f8afd1d73782685aea47875297c26a637b9
Chris McComb <ccmcc2012@gmail.com>
2022-05-09 13:39:37 -0400

    Verification slowly

--- examples/mbb_full_definition_with_settings.rs
+++ examples/mbb_full_definition_with_settings.rs
@@ -1,11 +1,10 @@
 use topopt::{solve, Settings};
 
 fn main() {
-    let settings = Settings::new(120, 30, 0.5)
+    let settings = Settings::new(60, 20, 0.5)
         .with_bottom_right_bc(false, true)
-        .with_bottom_left_bc(false, true)
-        .with_vertical_midline_bc(true, false)
-        .with_top_middle_load(0.0, -1.0);
+        .with_left_bc(true, false)
+        .with_top_left_load(0.0, -1.0);
 
     // Solve
     solve(settings);
