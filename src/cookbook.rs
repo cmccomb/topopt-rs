e06b0c1d44c46a8a6c28068dd0590e7d73c29e07
Chris McComb <ccmcc2012@gmail.com>
2022-05-13 13:24:26 -0400

    The cookbook begins

--- src/cookbook.rs
+++ src/cookbook.rs
@@ -1,6 +1,6 @@
 //! # The Cookbook
 //! # Beaucoup Boundary Conditions and Lotsa Loads
-//! # Messerschmitt–Bölkow–Blohm Simply Supported Beam (with symmetry)
+//! ## Messerschmitt–Bölkow–Blohm Simply Supported Beam (with symmetry)
 //! ```rust
 //! let settings = topopt::Settings::new(60, 20, 0.5)
 //!     .with_left_bc(true, false)
@@ -9,7 +9,7 @@
 //!
 //! topopt::solve(settings);
 //! ```
-//! # Messerschmitt–Bölkow–Blohm Simply Supported Beam (without symmetry)
+//! ##  Messerschmitt–Bölkow–Blohm Simply Supported Beam (without symmetry)
 //! ```rust
 //! let settings = topopt::Settings::new(120, 20, 0.5)
 //!     .with_bottom_right_bc(false, true)
